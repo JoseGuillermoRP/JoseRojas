06c5be018b31233ed4fdd15965c7621aca75d29e
Jose Rojas Paredes <jrojasp1992@gmail.com>
2020-03-21 14:10:29 -0500

    cambio contacto3

--- js/form.js
+++ js/form.js
@@ -4,6 +4,6 @@ document.querySelector('#enviar').addEventListener('click',function(){
     let email = document.querySelector('#email').value;
     let mensaje = document.querySelector('#mensaje').value;
 
-    let url = "https://api.whatsapp.com/send?phone=969198897&text=*Informacion*%0A*de*%0A*Contacto*%0A%0A*Nombre:*%0A" + nombre + "%0A*Numero*%0A*de*%0A*Celular:*%0A" + numerocelular + "%0A*Correo*%0A*electronico:*%0A" + email + "%0A*Mensaje:*%0A" + mensaje;
+    let url = "https://api.whatsapp.com/send?phone=+51969198897&text=*Informacion*%0A*de*%0A*Contacto*%0A%0A*Nombre:*%0A" + nombre + "%0A*Numero*%0A*de*%0A*Celular:*%0A" + numerocelular + "%0A*Correo*%0A*electronico:*%0A" + email + "%0A*Mensaje:*%0A" + mensaje;
     window.open(url);
 });
